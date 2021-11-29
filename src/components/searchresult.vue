6d87d6ccc8eba541a174dd82952419791d86e53a
meow0516 <76482269+meow0516@users.noreply.github.com>
2021-11-29 18:49:04 +0800

    add component: searchresult

---
+++ src/components/searchresult.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="search-bar">
+    abc
+  </div>
+</template>
+
+<script>
+export default {
+  name: "searchresult",
+  props: {
+    
+  },
+  data: ()=>({
+    
+  }),
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
