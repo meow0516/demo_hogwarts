0b594524c13da9899eee1af2c06fd6c498a57933
meow0516 <76482269+meow0516@users.noreply.github.com>
2021-11-29 15:05:12 +0800

    add homepage(introduction) and other pages

---
+++ src/components/introduction.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="introduction">
+    <input type="radio" id="text" value="text" v-model="dataType">
+    <label for="text">text</label>
+
+    <input type="text">
+    <input type="radio">
+    <select name="" id=""></select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "introduction",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <introduction/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import introduction from '@/components/introduction.vue';
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    // HelloWorld,
+    introduction,
   },
 };
 </script>
