93d505a15ff3689727b040db7b8254825aaec12f
meow0516 <76482269+meow0516@users.noreply.github.com>
2021-11-30 11:41:24 +0800

    add image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="HogwartsLogo" src="../assets/images/Hogwarts-Logo-PNG-Image-Free-Download.png" />
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
     <introduction/>
   </div>
