b075811fc03093234b8672f7ff96f19a1e51bc6c
meow0516 <76482269+meow0516@users.noreply.github.com>
2021-12-02 16:37:29 +0800

    app.vue: add themecolor, save themecolor to cookies, update css

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" :class="themeColor">
+    <div class="theme-select">Choose theme color
+      <input type="radio" id="theme_default" value="defaultTheme" v-model="themeColor">
+      <label for="theme_default">Default</label>
+
+      <input type="radio" id="theme_red" value="redTheme" v-model="themeColor">
+      <label for="theme_red">Red</label>
+
+      <input type="radio" id="theme_yellow" value="yellowTheme" v-model="themeColor">
+      <label for="theme_yellow">Yellow</label>
+
+      <input type="radio" id="theme_blue" value="blueTheme" v-model="themeColor">
+      <label for="theme_blue">Blue</label>
+
+      <input type="radio" id="theme_green" value="greenTheme" v-model="themeColor">
+      <label for="theme_green">Green</label>
+    </div>
+    <div id="nav" class="nav">
+      <router-link to="/">Home</router-link> 
       <router-link to='/Gryffindor'>Gryffindor</router-link>
       <router-link to='/Hufflepuff'>Hufflepuff</router-link>
       <router-link to='/Ravenclaw'>Ravenclaw</router-link>
@@ -16,29 +31,64 @@
 export default {
   mounted(){
     this.$store.dispatch('getList')
+    this.themeColor = this.$cookies.get('themeColor')
+  },
+  data: ()=>({
+    themeColor:'defaultTheme',
+  }),
+  watch:{
+    themeColor: function(){
+      this.$cookies.set('themeColor',this.themeColor)
+    }
   }
 }
 </script>
 
 <style lang="scss">
+body{
+  padding: 0;
+  margin: 0;  
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100vh;
+  padding: 30px;
 }
 
+.defaultTheme{
+  background-color: #FCF5E5;
+}
+.redTheme{
+  background-color: #F0C8C8;
+}
+.yellowTheme{
+  background-color: #FAF0BE;
+}
+.blueTheme{
+  background-color: #C8C8F0;
+}
+.greenTheme{
+  background-color: #D2F0C8;
+}
 #nav {
   padding: 30px;
-
+  
   a {
     font-weight: bold;
     color: #2c3e50;
-
+    padding: 10px 20px;
     &.router-link-exact-active {
       color: #42b983;
     }
   }
 }
+
+label{
+  padding: 5px 10px;
+}
+
 </style>
