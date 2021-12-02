d47c35933972af9f115fb4f08f42d5cd205ca03d
meow0516 <76482269+meow0516@users.noreply.github.com>
2021-12-02 19:42:41 +0800

    setting meta info

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import store from "./store";
 import axios from "axios"
 import VueAxios from "vue-axios"
 import VueCookies from "vue-cookies"
+import MetaInfo from 'vue-meta-info'
 
 Vue.config.productionTip = false;
 Vue.use(VueAxios, axios)
 Vue.use(VueCookies)
 Vue.$cookies.config('7d')
+Vue.use(MetaInfo)
 
 new Vue({
   router,
