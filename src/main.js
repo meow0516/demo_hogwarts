51fe1577518eef87e8743887ad17b5051157e808
meow0516 <76482269+meow0516@users.noreply.github.com>
2021-12-02 16:34:05 +0800

    setting cookies

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import axios from "axios"
+import VueAxios from "vue-axios"
+import VueCookies from "vue-cookies"
 
 Vue.config.productionTip = false;
 Vue.use(VueAxios, axios)
+Vue.use(VueCookies)
+Vue.$cookies.config('7d')
 
 new Vue({
   router,
